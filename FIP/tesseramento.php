5a27d6bb66238f785f44a279542c0fcd23765697
VincyLuppino <65389403+VincyLuppino@users.noreply.github.com>
2021-05-08 11:55:51 +0200

    aggiornamento v9
    modificato indexSocieta
    aggiunto iscriviGiocatore
    aggiunto tesseramento

---
+++ FIP/tesseramento.php
@@ -0,0 +1,123 @@
+<head>
+
+
+
+    <!-- Favicons -->
+    <link href="assets/img/faviconFIP.jpg" rel="icon"> <!-- Logo in alto a sinistra -->
+    <link href="assets/img/faviconFIP.jpg" rel="faviconFIP.jpg">
+
+
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/styleTesseramento.css" rel="stylesheet">
+
+
+
+
+
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+
+
+    <link rel="icon" href="Favicon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <title>Pagina di registrazione</title>
+
+    <link href="assets/css/styleTesseramento.css" rel="stylesheet">
+
+
+
+
+</head>
+
+<body style="background-image: url('assets/img/basketBackground.jpg')">
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top">
+        <div class="container d-flex align-items-center">
+
+            <h1 class="logo mr-auto"><a href="indexSocieta.php"><img src="assets/img/logoFIP.jpg"></a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+            <nav class="nav-menu d-none d-lg-block">
+                <ul>
+                    <li class="drop-down"><a href="">Gestione giocatori</a>
+                        <ul>
+                            <li><a href="iscriviGiocatore.php">Nuovo giocatore</a></li>
+                            <li><a href="tesseramento.php">Tesseramento</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="iscrizioneCampionati.php">Iscrizione campionati</a></li>
+
+                </ul>
+            </nav><!-- .nav-menu -->
+
+
+
+        </div>
+    </header><!-- End Header -->
+
+
+
+    <section style="margin-top: 100px; margin-left: 200px; background-color: white;">
+        <div class="container">
+
+            <table class="table table-striped custab" style="width: auto;">
+                <thead>
+                    <tr>
+                        <th>N.giocatore</th>
+                        <th>Nome</th>
+                        <th>Cognome</th>
+                        <th class="text-center">Action</th>
+                    </tr>
+                </thead>
+                <tr>
+                    <td>1</td>
+                    <td>Vincenzo</td>
+                    <td>Luppino</td>
+                    <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-edit"></span> Tessera</a> </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Vincenzo</td>
+                    <td>Luppino</td>
+                    <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-edit"></span> Tessera</a> </td>
+                </tr>
+
+            </table>
+
+        </div>
+
+    </section>
+
+</body>
